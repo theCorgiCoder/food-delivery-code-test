489b0f127f87eac969db29688a1982f155c373cb
Elaine B. <elainebergstrom26@gmail.com>
2024-10-19 17:26:40 +0200

    Base navigation set up for screens and screen folders

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  // Redirect to home screen when the app starts
+  return <Redirect href="/screens/home/HomeScreen" />;
 }
